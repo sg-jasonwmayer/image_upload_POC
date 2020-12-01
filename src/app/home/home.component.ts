13158a5cd49c9d0337010f8b27e7c70954c77cfc
Jason Mayer <flavorflavor@gmail.com>
2020-12-01 14:20:55 -0500

    home page update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
+import { HttpEventType, HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { UploadService } from  '../upload.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+    @ViewChild('fileUpload', { static: false })
+  fileUpload!: ElementRef; files  = [];
+    constructor(private uploadService: UploadService) { }  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
-
+  uploadFile(file: { data: string | Blob; inProgress: boolean; progress: number; }) {
+    const formData = new FormData();
+    formData.append('file', file.data);
+    file.inProgress = true;
+    this.uploadService.upload(formData).pipe(
+      map(event => {
+        switch (event.type) {
+          case HttpEventType.UploadProgress:
+            file.progress = Math.round(event.loaded * 100 / event.total);
+            break;
+          case HttpEventType.Response:
+            return event;
+        }
+      }),
+      catchError((error: HttpErrorResponse) => {
+        file.inProgress = false;
+        return of(`${file.data.name} upload failed.`);
+      })).subscribe((event) => {
+        if (typeof (event) === 'object') {
+          console.log(event.body);
+        }
+      });
+  }
+  private uploadFiles() {
+    this.fileUpload.nativeElement.value = '';
+    this.files.forEach(file => {
+      this.uploadFile(file);
+    });
+}
+onClick() {
+  const fileUpload = this.fileUpload.nativeElement; fileUpload.onchange = () => {
+  for (let index = 0; index < fileUpload.files.length; index++)
+  {
+   const file = fileUpload.files[index];
+   this.files.push({ data: file, inProgress: false, progress: 0});
+  }
+  this.uploadFiles();
+  };
+  fileUpload.click();
+}
 }
---
+++ src/app/upload.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpErrorResponse, HttpEventType } from  '@angular/common/http';
+import { map } from  'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+	SERVER_URL = 'https://file.io/';
+  constructor(private httpClient: HttpClient) { }
+  public upload(formData: any) {
+    return this.httpClient.post<any>(this.SERVER_URL, formData, {
+        reportProgress: true,
+        observe: 'events'
+      });
+  }
+}
